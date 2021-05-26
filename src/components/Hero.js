c1097bea6b52c8b1aa4dc45c0d7a2c097c74997d
Julian Almendarez <ppalmendarez@yahoo.com>
2021-05-26 11:50:38 -0500

    fixes navbar and background sahde for image

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,7 +8,7 @@ const Hero = () => {
     <Jumbotron
       fluid
       style={{
-        backgroundImage: `url(https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/f49e62e3-9d09-4452-a762-f750621d25bd/d77gofi-b6abe652-def0-445e-be4d-da2391b402a2.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2Y0OWU2MmUzLTlkMDktNDQ1Mi1hNzYyLWY3NTA2MjFkMjViZFwvZDc3Z29maS1iNmFiZTY1Mi1kZWYwLTQ0NWUtYmU0ZC1kYTIzOTFiNDAyYTIucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.RLHe-LbZD7piYqYX9WM9KuNSVuEz6mf0aNfm-ndfr2k)`,
+        backgroundImage:`url(https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/f49e62e3-9d09-4452-a762-f750621d25bd/d77gofi-b6abe652-def0-445e-be4d-da2391b402a2.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2Y0OWU2MmUzLTlkMDktNDQ1Mi1hNzYyLWY3NTA2MjFkMjViZFwvZDc3Z29maS1iNmFiZTY1Mi1kZWYwLTQ0NWUtYmU0ZC1kYTIzOTFiNDAyYTIucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.RLHe-LbZD7piYqYX9WM9KuNSVuEz6mf0aNfm-ndfr2k)`,
         backgroundSize: "cover",
       }}
     >
--- src/components/NavbarElement.js
+++ src/components/NavbarElement.js
@@ -5,16 +5,20 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 
 const NavbarElement = () => {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="danger" variant="dark">
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="mx-auto">
+        <Nav
+          className="mr-auto"
+          style={{
+            fontWeight: "bold",
+          }}
+        >
           <Navbar.Brand href="https://github.com/stbxzr">Git Hub</Navbar.Brand>
           <Navbar.Brand href="https://www.linkedin.com/in/julian-almendarez-940a6a206">
             LinkedIn
           </Navbar.Brand>
           <Navbar.Brand href="https://drive.google.com/file/d/1pcGeynmwMVmqSE3INJULaQw7j-gaT_YM/view?usp=sharing">
-           
             Resume/Contact
           </Navbar.Brand>
         </Nav>
